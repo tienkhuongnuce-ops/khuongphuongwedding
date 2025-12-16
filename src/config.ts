49fe61445d6bac1c82d96b101e3f7c086dfaeb63
Tien Khuong <tienkhuong.nuce@gmail.com>
2025-12-16 23:39:00 +0700

    update photos

--- src/config.ts
+++ src/config.ts
@@ -74,7 +74,6 @@ export const weddingConfig = {
     gallery: [
       "/gallery1.jpg",
       "/gallery2.jpg",
-      "/gallery3.jpg",
       "/gallery4.jpg",
       "/gallery5.jpg",
       "/gallery6.jpg",
